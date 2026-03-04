17f364f0edada3c3406a7291bc9ce918c1faf646
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-03-04 11:39:51 +0000

    fix: don't use fileset

--- package.nix
+++ package.nix
@@ -4,21 +4,11 @@ buildGoModule {
   pname = "go2nix";
   version = "0-unstable";
 
-  src = lib.fileset.toSource {
-    root = ./go2nix;
-    fileset = lib.fileset.unions [
-      ./go2nix/go.mod
-      ./go2nix/go.sum
-      ./go2nix/main.go
-      ./go2nix/main_test.go
-      ./go2nix/integration_test.go
-      ./go2nix/builder
-    ];
-  };
+  src = ./go2nix;
 
   subPackages = [ "." ];
 
-  vendorHash = "sha256-xC7TFbsTp0YsIXhRO9LLZwLTW5rU9GRFTMOBbTMSbns=";
+  vendorHash = "sha256-kESsE+x8ca+9HL6ce9epmStvAvM13vO28iPlyLgguH8=";
 
   meta = {
     description = "Generate Nix lockfiles for Go modules";
